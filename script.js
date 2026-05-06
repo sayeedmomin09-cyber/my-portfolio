ce67b310d72120dd2a2665d790e3263b51764675
sayeedmomin09-cyber <sayeedmomin09@gmail.com>
2026-05-07 00:31:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -32,22 +32,24 @@ reveals.forEach(r => observer.observe(r));
 
 // CONTACT FORM
 async function sendMessage() {
-  alert("Button working");
-  const name = document.querySelector('input[type="text"]').value.trim();
-
-const email = document.querySelector('input[type="email"]').value.trim();
-
-const subject = document.querySelectorAll('input[type="text"]')[1].value.trim();
-
-const message = document.querySelector('textarea').value.trim();
+  alert();
+ 
 
   const status = document.getElementById('form-status');
 
   if (!name || !email || !message) {
     status.style.color = '#ff6b6b';
     status.textContent = '⚠️ Please fill all required fields.';
+    const name = document.getElementById('fname').value.trim();
+
+const email = document.getElementById('femail').value.trim();
+
+const subject = document.getElementById('fsubject').value.trim();
+
+const message = document.getElementById('fmessage').value.trim();
     return;
   }
+  
 
   try {
     const response = await fetch('https://my-portfolio-ifxh.onrender.com/contact', {
