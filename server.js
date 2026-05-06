55d090d6b5aa0cd91d604184ef7eb146f8951871
sayeedmomin09-cyber <sayeedmomin09@gmail.com>
2026-05-07 01:00:22 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const path = require('path');
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, '..')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
