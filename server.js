d0e94a550126702e37d641127b8780ff11d34954
sayeedmomin09-cyber <sayeedmomin09@gmail.com>
2026-05-06 22:41:46 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 const app = express();
-const path = require("path");
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..')));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,"index.html"));
+  res.sendFile(path.join(__dirname,'index.html'));
 });
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -19,6 +19,7 @@ const transporter = nodemailer.createTransport({
     pass:"fneenmhfdvuhyywv"
   }
 });
+
 
 
 mongoose.connect("mongodb://portfolioUser:Portfolio123@ac-ni5bh2h-shard-00-00.1rvb4fe.mongodb.net:27017,ac-ni5bh2h-shard-00-01.1rvb4fe.mongodb.net:27017,ac-ni5bh2h-shard-00-02.1rvb4fe.mongodb.net:27017/?ssl=true&replicaSet=atlas-nd2q4l-shard-0&authSource=admin&appName=Cluster0")
