984557fd911a3b2d08ef759016aba29dfcf06aaf
sayeedmomin09-cyber <sayeedmomin09@gmail.com>
2026-05-06 22:07:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,"..","index.html"));
+  res.sendFile(path.join(__dirname,"index.html"));
 });
 const transporter = nodemailer.createTransport({
   service: "gmail",
